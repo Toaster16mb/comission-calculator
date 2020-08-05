a7da6cadcb842e2e9e3bfef4f7dbff8809562b3a
s.alekseev <toaster16mb@gmail.com>
2020-08-05 03:25:47 +0300

    fix

--- app/Test/BinResultsLookupBinlistTest.php
+++ app/Test/BinResultsLookupBinlistTest.php
@@ -8,8 +8,7 @@ class testBinResultsLookupBinlist extends \PHPUnit\Framework\TestCase
 {
     public function testGetCountryCodeByBin() {
         $fileContentGetter = $this->createMock(FileContentGetter::class);
-        $fileContentGetter
-            ->expects($this->atLeastOnce())
+        $fileContentGetter->expects($this->atLeastOnce())
             ->method('fileGetContents')
             ->willReturn('{"number":{"length":16,"luhn":true},"scheme":"visa","type":"debit","brand":"Visa/Dankort","prepaid":false,"country":{"numeric":"208","alpha2":"DK","name":"Denmark","emoji":"��","currency":"DKK","lat  itude":56,"longitude":10},"bank":{"name":"Jyske Bank","url":"www.jyskebank.dk","phone":"+4589893300","city":"Hjørring"}}');
         $binResultsLookupBinlist = new BinResultsLookupBinlist($fileContentGetter);
